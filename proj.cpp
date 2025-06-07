92378ee2285eb0002b78d65920fba768a2a615a6
Vidalb1 <vidalbickersteth@gmail.com>
2025-06-07 18:46:38 -0400

    Update proj.cpp

--- proj.cpp
+++ proj.cpp
@@ -8,9 +8,6 @@
 ** This program is the proj.cpp file of the Text Editor Project.
 ** This file creates a textEditor object.
 ** The program implements the Text Editor.
-** The user can insert, delete, edit, display, search,
-** save, or load their lines.
-** The user may also exit the text editor if they wish.
 ***********************************************/
 #include "TextEditor.h"
 #include <iostream>
